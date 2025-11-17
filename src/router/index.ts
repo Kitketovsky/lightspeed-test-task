b65b01a6ac070618f0abe2b0f318852ad6615eaf
kitketovsky <kitketovsky@gmail.com>
2025-11-18 02:02:47 +0400

    added router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,20 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory, type RouterOptions } from 'vue-router'
+
+import HomePage from '../pages/HomePage/HomePage.vue'
+import ProductPage from '../pages/ProductPage.vue'
+import CartPage from '../pages/CartPage.vue'
+import NotFoundPage from '../pages/NotFoundPage.vue'
+
+const routes: RouterOptions['routes'] = [
+  { path: '/', component: HomePage, name: 'home' },
+  { path: '/product/:id', component: ProductPage, name: 'product' },
+  { path: '/cart', component: CartPage, name: 'cart' },
+  { path: '/:pathMatch(.*)*', component: NotFoundPage, name: 'not-found' },
+]
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes,
 })
 
 export default router
