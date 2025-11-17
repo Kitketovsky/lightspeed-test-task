5edff47c6c26189054f6886c094db6e06e6ec9c8
kitketovsky <kitketovsky@gmail.com>
2025-11-18 01:58:12 +0400

    added api layer

---
+++ src/api/products.ts
@@ -0,0 +1,30 @@
+import type { http as HttpType } from './http'
+import type {
+  ApiGetProductResponse,
+  ApiGetProductsResponse,
+} from '../types/products'
+
+export default (http: typeof HttpType) => ({
+  async getProducts({
+    categoryId,
+  }: { categoryId?: string | number | null } = {}) {
+    const query = {
+      responseFields:
+        'items(id,name,description,imageUrl,thumbnailUrl,price,defaultDisplayedPriceFormatted,defaultCategoryId,categoryIds)',
+      includeProductsFromSubcategories: true,
+      ...(categoryId && { category: categoryId }),
+    }
+
+    if (categoryId) {
+      query.category = categoryId
+    }
+
+    return await http<ApiGetProductsResponse>('/products', {
+      query,
+    })
+  },
+
+  async getProduct({ id }: { id: number }) {
+    return await http<ApiGetProductResponse>(`/products/${id}`)
+  },
+})
